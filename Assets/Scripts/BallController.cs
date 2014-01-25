2a577c56f93a2c79eeb6d98cb8750a074a75a92a
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2014-01-25 23:35:01 +0100

    Added ball puzzle to cat hallway.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : WithMouseActions
+{
+
+
+		public Vector3 targetPosition;
+		bool wasTriggered = false;
+
+		public override void PerformOnClickAction ()
+		{
+				if (!wasTriggered) {
+						wasTriggered = true;
+						StartCoroutine (MoveToTarget (targetPosition));
+				}
+
+		}
+
+		IEnumerator MoveToTarget (Vector3 target)
+		{
+				while (Vector3.Distance(transform.position, target) > 0.05f) {
+						transform.position = Vector3.Lerp (transform.position, target, 1f * Time.deltaTime);
+			
+						yield return null;
+				}
+		
+				print ("Reached the target.");
+				GuardController guard = GameObject.Find ("guard").GetComponent<GuardController> ();
+				guard.Distract ();
+		}
+
+}
