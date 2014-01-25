22e0809a0dd9c3e2f691e4b6095dcb2457153c4a
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2014-01-25 21:49:52 +0100

    Added guard controller, and separated guard from background.

---
+++ Assets/Scripts/Objects/GuardController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardController : MonoBehaviour {
+
+	bool isDistracted = false;
+
+	public Sprite GuardWatching;
+	public Sprite GuardDistracted;
+
+	public bool IsDistracted(){
+		return isDistracted;
+		} 
+
+	void Start () {
+		GetComponent<SpriteRenderer> ().sprite = GuardWatching;
+	}
+
+	public void Distract() {
+		GetComponent<SpriteRenderer> ().sprite = GuardDistracted;
+		isDistracted = true;
+	}
+}
