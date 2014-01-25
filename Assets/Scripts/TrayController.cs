2f2f4ed221e30ed6eae3a610c83c411d88435597
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2014-01-25 01:19:32 +0100

    Started building the first room.

---
+++ Assets/Scripts/TrayController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrayController : Clickable {
+
+	public Sprite whole;
+	public Sprite broken;
+
+	void Start() {
+		GetComponent<SpriteRenderer> ().sprite = whole;
+	}
+
+	protected override void PerformClickAction ()
+	{
+		base.PerformClickAction ();
+		
+		GetComponent<SpriteRenderer> ().sprite = broken;
+		GameObject key = GameObject.FindWithTag ("Key");
+		key.transform.position = new Vector3 (2.9f, -2.73f, 0.0f);
+	}
+}
