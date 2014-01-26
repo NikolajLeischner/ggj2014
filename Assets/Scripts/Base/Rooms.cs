d4a6755509b08a53ad845b055b62f71786c15a2b
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2014-01-26 14:11:16 +0100

    Updated graphics for lobby.

--- Assets/Scripts/Base/Rooms.cs
+++ Assets/Scripts/Base/Rooms.cs
@@ -11,8 +11,11 @@ public class Rooms  {
 
 	public static string Lobby = "Lobby";
 
+	public static string Outro = "Outro";
+
 	public static string SceneForCharacter(string scene, Character character) {
 		if (scene == Rooms.Bedroom) return scene; 
+else if (scene == Rooms.Outro) return scene;
 		else
 		return scene + character.SceneSuffix;
 	}
