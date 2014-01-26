f6b104dd13303d89d8ceddd182fae075e10bb25c
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2014-01-26 01:22:51 +0100

    Bedroom door opens before moving to the next room..

--- Assets/Scripts/Objects/DoorController.cs
+++ Assets/Scripts/Objects/DoorController.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DoorController : WithMouseActions {
+public class DoorController : WithMouseActions
+{
 
-	public override void PerformOnClickAction() {
-		InventoryItem key = GameObject.Find ("key").GetComponent<InventoryItem> ();
-		if (key.IsActive ()) {
-						roomManager.MoveToNextRoom ();		
+		public Sprite BedroomOpen;
+		bool wasTriggered = false;
+
+		public override void PerformOnClickAction ()
+		{
+				InventoryItem key = GameObject.Find ("key").GetComponent<InventoryItem> ();
+				if (!wasTriggered && key.IsActive ()) {
+						wasTriggered = true;
+						StartCoroutine (OpenDoorAndMove ());	
 				} else {
-			roomManager.AddInfoText ("It is not *that* easy!");
+						roomManager.AddInfoText ("It is not *that* easy!");
 				}
-	}
+		}
+
+		IEnumerator OpenDoorAndMove ()
+		{
+				GameObject.Find ("bedroom").GetComponent<SpriteRenderer> ().sprite = BedroomOpen;
+				yield return new WaitForSeconds (2);
+				roomManager.MoveToNextRoom ();	
+		}
 }
